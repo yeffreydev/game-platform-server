0f717a951fdb2d092e8083ec06feae8304b9680c
yeffreycode <yeffreyespinoza200@gmail.com>
2023-04-03 12:17:23 -0500

    create files from fly and modified default port

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  PORT: process.env.PORT || 4000,
+  PORT: process.env.PORT || "8080",
   MONGO_URI: process.env.MONGO_URI || "",
   JWT_SECRET: process.env.JWT_SECRET || null,
   ORIGIN: process.env.ORIGIN,
