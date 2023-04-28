0a88cc289df2edb91405fe3a7c84615714196b52
yeffreycode <yeffreyespinoza200@gmail.com>
2023-04-27 20:37:54 -0500

    ignore folders for no compile

---
+++ test/index.test.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const online_paint_1 = require("../src/lib/data/online_paint");
+// addNewPaintSocket("hfow83894283942", "8329989fa");
+(0, online_paint_1.removePaintSocket)("hfow83894283942", "8329989fs");
